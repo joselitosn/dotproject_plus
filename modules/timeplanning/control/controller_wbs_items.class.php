7b8d6c32c794c3f5d80196ae72131efaef1f5ca3
Hillesheim <jfernandohillesheim@luc.edu>
2019-10-15 23:14:17 -0300

    Implementado workaround para mostrar os itens da eap de acordo para exibicao

--- modules/timeplanning/control/controller_wbs_items.class.php
+++ modules/timeplanning/control/controller_wbs_items.class.php
@@ -45,20 +45,36 @@ class ControllerWBSItem {
             }
             return $WBSItem;
 	}
-	
-        
         
 	function getWBSItems($projectId){
+
+        function sortWBSItemsByOrder($a, $b) {
+            return $a['sort_order'] - $b['sort_order'];
+        }
+
 		$list=array();
 		$q = new DBQuery();
 		$q->addQuery('t.id, t.item_name,t.identation,t.number,t.is_leaf,t.sort_order, ie.size, ie.size_unit');
 		$q->addTable('project_eap_items', 't');
         $q->addJoin('eap_item_estimations', 'ie', 'ie.eap_item_id = t.id');
-		$q->addWhere('project_id = '.$projectId .' order by sort_order');
+		$q->addWhere('project_id = '.$projectId .' order by number');
 		$sql = $q->prepare();
 		$items = db_loadList($sql);
-                $i=0;
-		foreach ($items as $item) {
+
+		// Workaround. needs to recalculate the sort order in the database for each item.
+        // This is not reliable as the sort order is used to create new items.
+        // Temporary for user display only.
+        $j=0;
+        foreach ($items as $item) {
+            $number = explode(".", $item['number']);
+            $items[$j]['sort_order'] = array_sum($number);
+            $j++;
+        }
+
+        usort($items, 'sortWBSItemsByOrder');
+
+        $i=0;
+        foreach ($items as $item) {
 			$id = $item['id'];
 			$name = $item['item_name'];
 			$identation= $item['identation'];
@@ -72,8 +88,10 @@ class ControllerWBSItem {
 			$list[$i]=$WBSItem;
             $i++;
 		}
+
 		return $list;
 	}
+
 	
 	function getWorkPackages($projectId){
 		$list=array();
